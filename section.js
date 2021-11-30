37bc3a558ae6ff618881316576b134d83263eb73
Hoang Bui <hoangict@hotmail.com>
2021-11-30 10:20:19 +0700

    Update i18n

--- section.js
+++ section.js
@@ -1,18 +1,14 @@
-(function (blocks, element, blockEditor, components) {
-    var el = element.createElement,
-            Fragment = element.Fragment,
-            useBlockProps = blockEditor.useBlockProps,
-            InnerBlocks = blockEditor.InnerBlocks,
-            InspectorControls = blockEditor.InspectorControls,
-            MediaUpload = blockEditor.MediaUpload;
+(function (blocks, element, blockEditor, components, i18n) {
+    var el = element.createElement;
 
-    var PanelBody = components.PanelBody,
-            Button = components.Button,
-            SelectControl = components.SelectControl;
+    const {__} = i18n;
+    const {Fragment} = element;
+    const {useBlockProps, InnerBlocks, InspectorControls, MediaUpload} = blockEditor;
+    const {PanelBody, Button, SelectControl} = components;
 
     blocks.registerBlockType('main/section', {
         apiVersion: 2,
-        title: 'Section',
+        title: __('Section', 'viweb'),
         icon: 'align-wide',
         category: 'design',
         keywords: ['section', 'container'],
@@ -80,16 +76,16 @@
                 });
             }
             let repeatOpts = [
-                {label: 'Auto', value: 'auto'},
-                {label: 'No Repeat', value: 'no-repeat'},
-                {label: 'Repeat X', value: 'repeat-x'},
-                {label: 'Repeat Y', value: 'repeat-y'}
+                {label: __('Auto'), value: 'auto'},
+                {label: __('No Repeat', 'viweb'), value: 'no-repeat'},
+                {label: __('Repeat X', 'viweb'), value: 'repeat-x'},
+                {label: __('Repeat Y', 'viweb'), value: 'repeat-y'}
             ];
             let sizeOpts = [
-                {label: 'Auto', value: 'auto'},
-                {label: 'Cover', value: 'cover'},
-                {label: 'Contain', value: 'contain'},
-                {label: 'Full', value: '100%'}
+                {label: __('Auto'), value: 'auto'},
+                {label: __('Cover', 'viweb'), value: 'cover'},
+                {label: __('Contain', 'viweb'), value: 'contain'},
+                {label: '100%', value: '100%'}
             ];
             if (bgImageUrl) {
                 blockProps.style.backgroundImage = 'url("' + bgImageUrl + '")';
@@ -102,28 +98,28 @@
             }
             let focus = props.focus;
 
-            return el(Fragment, {}, el(InspectorControls, {}, el(PanelBody, {title: 'Select Background Image'},
-                    el('div', {className: 'components-base-control'}, el('label', {className: 'components-form-token-field__label'}, 'Background Image'), el(MediaUpload, {
+            return el(Fragment, {}, el(InspectorControls, {}, el(PanelBody, {title: __('Select Background Image', 'viweb')},
+                    el('div', {className: 'components-base-control'}, el('label', {className: 'components-form-token-field__label'}, __('Background Image', 'viweb')), el(MediaUpload, {
                         onSelect: onChangeImage,
                         allowedTypes: 'image/*',
                         value: bgImageId,
                         render: function (obj) {
                             if (bgImageId) {
-                                return el('div', {}, el('img', {src: bgImageUrl}), el('div', {}, el(Button, {className: 'is-secondary', onClick: obj.open}, 'Change Image'), el(Button, {className: 'is-link is-destructive', onClick: onRemoveImage}, 'Remove Image')));
+                                return el('div', {}, el('img', {src: bgImageUrl}), el('div', {}, el(Button, {className: 'is-secondary', onClick: obj.open}, __('Change Image', 'viweb')), el(Button, {className: 'is-link is-destructive', style: {'margin-left': '10px'}, onClick: onRemoveImage}, __('Remove Image', 'viweb'))));
                             } else {
                                 return el(Button, {
                                     className: 'editor-post-featured-image__toggle',
                                     onClick: obj.open
-                                }, 'Browser Image');
+                                }, __('Select Image', 'viweb'));
                             }
                         }
                     })),
-                    el(SelectControl, {label: 'Background repeat', options: repeatOpts, value: bgImageRepeat, onChange: function (val) {
+                    el(SelectControl, {label: __('Background repeat', 'viweb'), options: repeatOpts, value: bgImageRepeat, onChange: function (val) {
                             props.setAttributes({
                                 bgImageRepeat: val
                             });
                         }}),
-                    el(SelectControl, {label: 'Background size', options: sizeOpts, value: bgImageSize, onChange: function (val) {
+                    el(SelectControl, {label: __('Background size', 'viweb'), options: sizeOpts, value: bgImageSize, onChange: function (val) {
                             props.setAttributes({
                                 bgImageSize: val
                             });
@@ -158,5 +154,4 @@
             }
         }
     });
-})(window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.components);
-
+})(window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.components, window.wp.i18n);
