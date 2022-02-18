cf664b9bd0a0120c321a6bb34b201e985aaa29c2
Hoang Bui <hoangict@hotmail.com>
2022-02-18 11:46:43 +0700

    Set column in row

--- columns.js
+++ columns.js
@@ -1,85 +1,27 @@
-(function (blocks, element, blockEditor, components, i18n) {
+(function (blocks, element, blockEditor, i18n) {
     var el = element.createElement;
     const {__} = i18n;
     const {Fragment} = element;
-    const {useBlockProps, InnerBlocks, InspectorControls} = blockEditor;
-    const {RangeControl, PanelBody} = components;
-    blocks.registerBlockType('main/columns', {
+    const {useBlockProps, InnerBlocks} = blockEditor;
+
+    blocks.registerBlockType('main/column', {
         apiVersion: 2,
         title: __('Columns'),
         icon: 'schedule',
         category: 'design',
-        keywords: ['columns'],
-        attributes: {
-            col: {
-                type: 'number',
-                default: 2
-            },
-            xl: {
-                type: 'number',
-                default: 0
-            },
-            lg: {
-                type: 'number',
-                default: 0
-            },
-            md: {
-                type: 'number',
-                default: 0
-            },
-            sm: {
-                type: 'number',
-                default: 0
-            }
-        },
+        keywords: ['column'],
+        parent: 'main/row',
         supports: {
             anchor: true,
-            html: false,
-            color: {
-                background: true,
-                gradients: true,
-                link: true
-            }
+            html: false
         },
         edit: function (props) {
-            const {col, xl, lg, md, sm} = props.attributes;
             let blockProps = useBlockProps();
-            blockProps.className += ' has-' + col + '-columns';
-            const renderBlocks = () => {
-                let elements = [];
-                for (let i = 0; i < col; i++) {
-                    elements.push(el('div', {className: 'ws-column'}, el(InnerBlocks, {renderAppender: props.isSelected ? InnerBlocks.ButtonBlockAppender : undefined})));
-                }
-                return elements;
-            };
-
-            return el(Fragment, {}, el(InspectorControls, {}, el(PanelBody, {title: __('Select Columns', 'viweb')},
-                    el(RangeControl, {label: __('Column Default', 'viweb'), min: 1, max: 12, value: col, onChange: (val) => {
-                            props.setAttributes({col: val});
-                        }}),
-                    el('hr'),
-                    el(RangeControl, {label: __('Extra Large (Desktop)', 'viweb'), min: 0, max: 12, value: xl, onChange: (val) => {
-                            props.setAttributes({xl: val});
-                        }}),
-                    el('hr'),
-                    el(RangeControl, {label: __('Large (Tablet)', 'viweb'), min: 0, max: 12, value: lg, onChange: (val) => {
-                            props.setAttributes({lg: val});
-                        }}),
-                    el('hr'),
-                    el(RangeControl, {label: __('Medium (Responsive)', 'viweb'), min: 0, max: 12, value: md, onChange: (val) => {
-                            props.setAttributes({md: val});
-                        }}),
-                    el('hr'),
-                    el(RangeControl, {label: __('Small (Mobile)', 'viweb'), min: 0, max: 12, value: sm, onChange: (val) => {
-                            props.setAttributes({sm: val});
-                        }})
-                    )),
-                    el('div', blockProps, renderBlocks())
-                    );
+            return el('div', blockProps, el(InnerBlocks, {renderAppender: InnerBlocks.ButtonBlockAppender}));
         },
         save: function (props) {
             let blockProps = useBlockProps.save();
             return el(InnerBlocks.Content, blockProps);
         }
     });
-})(window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.components, window.wp.i18n);
+})(window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.i18n);
